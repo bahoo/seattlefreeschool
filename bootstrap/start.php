9a698f740b6a867313be7114fba280723f55f8d3
bahoo <jon.c.culver@gmail.com>
2014-06-10 21:04:42 -0700

    Adding webfaction machine as a production box.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('sherman', 'sherman.local'),
+   'production' => array('web219.webfaction.com')
 
 ));
 
